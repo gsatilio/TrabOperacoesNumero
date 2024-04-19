28448570086711137953cb175979b24f23b3222d
Guilherme <g.satilio@gmail.com>
2024-04-19 10:19:28 -0300

    primeiro commit

--- TrabOperacoesNumero/Program.cs
+++ TrabOperacoesNumero/Program.cs
@@ -1,58 +1,60 @@
-﻿float n1 = 0, n2 = 0, n3 = 0, n4 = 0, operacao = 0;
-string tipo = "", tipostr = "";
+﻿float n1 = 0, n2 = 0, n3 = 0, n4 = 0, resultado;
+int opcao;
 
-Console.WriteLine("Escolha o tipo de operação\n 1 - Adição\n 2 - Subtração\n 3 - Multiplicação\n 4 - Divisão");
-tipo = (Console.ReadLine()); // tipo = string para "aceitar" o null
-
-// Obriga o usuário a escolher uma opção válida
-while ((tipo != "1") && (tipo != "2") && (tipo != "3") && (tipo != "4"))
-{
-    Console.WriteLine("Por favor, escolha uma opção válida.");
-    tipo = (Console.ReadLine());
-}
-
-// Entrada de dados
-Console.WriteLine("Informe o primeiro numero");
+Console.WriteLine("Informe o primeiro valor:");
 n1 = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Informe o segundo numero");
-n2 = float.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe o terceiro numero");
-n3 = float.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe o quarto numero");
-n4 = float.Parse(Console.ReadLine());
-
-// Tratativa div por zero
-if ((tipo == "4") && ((n2 == 0) || (n3 == 0) || (n4 == 0)))
+while (n2 == 0)
 {
-    Console.WriteLine("Nao é possivel dividir por zero.");
+    Console.WriteLine("Informe o segundo valor:");
+    n2 = float.Parse(Console.ReadLine());
+}
+
+do
+{
+    Console.WriteLine("Informe o terceiro valor:");
+    n3 = float.Parse(Console.ReadLine());
+} while (n3 == 0);
+
+
+for (n4 = 0; n4 == 0; )
+{
+    Console.WriteLine("Informe o quarto valor:");
+    n4 = float.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Informe a operação desejada:");
+Console.WriteLine("1 - Adição:");
+Console.WriteLine("2 - Subtração:");
+Console.WriteLine("3 - Multiplicação:");
+Console.WriteLine("4 - Divisão:");
+
+opcao = int.Parse(Console.ReadLine());  
+
+if (opcao == 1)
+{
+    resultado = (n1 + n2 + n3 + n4);
+    Console.WriteLine("O resultado é: " + resultado);
 }
 else
 {
-    // Tipos de operações
-    if (tipo == "1")
+    if (opcao == 2)
     {
-        operacao = (n1 + n2 + n3 + n4);
-        tipostr = "Adição";
-    } else if (tipo == "2")
-    {
-        operacao = (n1 - n2 - n3 - n4);
-        tipostr = "Subtração";
-    } else if (tipo == "3")
-    {
-        operacao = (n1 * n2 * n3 * n4);
-        tipostr = "Multiplicação";
-    } else if (tipo == "4")
-    {
-        operacao = (n1 / n2 / n3 / n4);
-        tipostr = "Divisão";
+        resultado = (n1 - n2 - n3 - n4);
+        Console.WriteLine($"O resultado é {resultado}");
     }
-
-    // Resultado final
-    Console.WriteLine("Você escolheu o tipo de operação: " + tipostr + "\n" +
-                      "Para os numeros: " + n1 + ", " + n2 + ", " + n3 + ", " + n4 + "\n" +
-                      "O cálculo da operação é: " + operacao);
+    else
+    {
+        if (opcao == 3)
+        {
+            resultado = (n1 * n2 * n3 * n4);
+            Console.WriteLine("O resultado é: " + resultado);
+        }
+        else
+        {
+            resultado = (n1 / n2 / n3 / n4);
+            Console.WriteLine($"O resultado é {resultado}");
+        }
+    }
 }
-Console.ReadKey();
+
